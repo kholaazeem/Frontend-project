3f82a228e8ca75096d885f72a5f378491063d5ef
khola azeem <kholaazeem104@gmail.com>
2025-12-20 13:57:52 +0500

    in progress

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 import supabase from "./config.js";  
 
 
-            //  Sign Up Functionality
+            //  ============================================================Sign Up Functionality===========================================================================
 
  const signupBtn = document.getElementById("signup-btn");                    
  const signupEmail = document.getElementById("signup-email");
@@ -10,29 +10,34 @@ import supabase from "./config.js";
  const lastName = document.getElementById("last-name");
 
 
- if(!signupEmail){
-    Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'please enter a valid email address',
-        
-    });
-    return
- }
- if(!signupPass){
-    Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'please enter atleast six characters password',
-       
-    });
-     return
- }
 
-    signupBtn.addEventListener("click", async (e) => {  
+
+   signupBtn &&  signupBtn.addEventListener("click", async (e) => {  
         e.preventDefault();
 
-        const { data, error } = await supabase.auth.signUp(
+        
+ if(!signupEmail.value){
+  Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'please enter a valid email address',
+     
+  });
+  return
+}
+
+if(!signupPass.value){
+  Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'please enter atleast six characters password',
+     
+  });
+  return
+  
+}
+
+        const {data, error } = await supabase.auth.signUp(
   {
     email: signupEmail.value,
     password: signupPass.value,
@@ -40,15 +45,19 @@ import supabase from "./config.js";
       data: {
         first_name: firstName.value,
         last_name: lastName.value,
-        role: 'user'
+        role: 'user'   // for security purpose : so that only admin has access of  dashboard 
       }
     }
   }
 )
+
   if(data){
-    // console.log (data);
-    // console.log (data.user);
-    //  console.log (data.user.id);
+  
+      
+    
+    console.log (data);
+    console.log (data.user);
+     console.log (data.user.id);
     //  console.log (data.user.email);
     // console.log (data.user.user_metadata);
     // console.log(data.user.user_metadata.first_name);
@@ -58,7 +67,7 @@ import supabase from "./config.js";
     const { error } = await supabase
   .from('customers')
   .insert({
-    uid: data.user.id,
+    uid: data.user.id,  // store auth id in table so that login user connect with database
     first_name: data.user.user_metadata.first_name,
     last_name: data.user.user_metadata.last_name,
     email: data.user.email,
@@ -74,13 +83,45 @@ if(error){
 }else{
 
 alert("Signup successful! Please verify your email before logging in.");
-window.location.href = "login.html";
+window.location.href = "home.html";
 }
+    }
+
+  });
 
 
+  //  ======================================================== login functionality =================================================================================================
 
 
+  let loginEmail = document.getElementById("login-email")
+  let loginPass = document.getElementById("login-password")
+  let  loginBtn = document.getElementById("login-btn")
 
-  }
+
+  async  function login(){
+
+    e.preventDefault();
+
+        
+    if(!loginEmail.value){
+     Swal.fire({
+         icon: 'error',
+         title: 'Error',
+         text: 'please enter a valid email address',
+        
+     });
+     return
+   }
+   
+   if(!loginPass.value){
+     Swal.fire({
+         icon: 'error',
+         title: 'Error',
+         text: 'please enter atleast six characters password',
+        
+     });
+     return
+     
+   }
   
-    });
+  }
